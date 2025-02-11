75c3b7e259d2e45b40552086c1a91a8e051c6f1c
Justin Gardner <justin@rhynorater.com>
2025-02-11 06:01:29 -0500

    1.146.0

--- google-site-kit/includes/Core/Storage/Term_Meta.php
+++ google-site-kit/includes/Core/Storage/Term_Meta.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Class Google\Site_Kit\Core\Storage\Post_Meta
+ * Class Google\Site_Kit\Core\Storage\Term_Meta
  *
  * @package   Google\Site_Kit\Core\Storage
- * @copyright 2021 Google LLC
+ * @copyright 2025 Google LLC
  * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://sitekit.withgoogle.com
  */
@@ -11,69 +11,69 @@
 namespace Google\Site_Kit\Core\Storage;
 
 /**
- * Post metadata storage class.
+ * Term metadata storage class.
  *
- * @since 1.33.0
+ * @since 1.146.0
  * @access private
  * @ignore
  */
-final class Post_Meta implements Meta_Interface {
+final class Term_Meta implements Meta_Interface {
 
 	/**
-	 * Gets post meta.
+	 * Gets term meta.
 	 *
-	 * @since 1.33.0
+	 * @since 1.146.0
 	 *
-	 * @param int    $post_id Post ID.
+	 * @param int    $term_id Term ID.
 	 * @param string $key     Metadata key.
 	 * @param bool   $single  Whether to return a single value.
-	 * @return mixed Post meta value.
+	 * @return mixed Term meta value.
 	 */
-	public function get( $post_id, $key, $single = false ) {
-		return get_post_meta( $post_id, $key, $single );
+	public function get( $term_id, $key, $single = false ) {
+		return get_term_meta( $term_id, $key, $single );
 	}
 
 	/**
-	 * Updates a post meta field based on the given post ID.
+	 * Updates a term meta field based on the given term ID.
 	 *
-	 * @since 1.33.0
+	 * @since 1.146.0
 	 *
-	 * @param int    $post_id    Post ID.
+	 * @param int    $term_id    Term ID.
 	 * @param string $key        Metadata key.
 	 * @param mixed  $value      Metadata value.
 	 * @param mixed  $prev_value Previous value to check before updating. If specified, only update existing metadata entries with this value. Otherwise, update all entries.
 	 * @return bool TRUE on success, otherwise FALSE.
 	 */
-	public function update( $post_id, $key, $value, $prev_value = '' ) {
-		return update_post_meta( $post_id, $key, $value, $prev_value );
+	public function update( $term_id, $key, $value, $prev_value = '' ) {
+		return update_term_meta( $term_id, $key, $value, $prev_value );
 	}
 
 	/**
-	 * Adds a meta field to the given post.
+	 * Adds a meta field to the given term.
 	 *
-	 * @since 1.33.0
+	 * @since 1.146.0
 	 *
-	 * @param int    $post_id Post ID.
+	 * @param int    $term_id Term ID.
 	 * @param string $key     Metadata key.
 	 * @param mixed  $value   Metadata value.
 	 * @param bool   $unique  Whether the same key should not be added.
 	 * @return int|bool Meta id on success, otherwise FALSE.
 	 */
-	public function add( $post_id, $key, $value, $unique = false ) {
-		return add_post_meta( $post_id, $key, $value, $unique );
+	public function add( $term_id, $key, $value, $unique = false ) {
+		return add_term_meta( $term_id, $key, $value, $unique );
 	}
 
 	/**
-	 * Deletes a post meta field for the given post ID.
+	 * Deletes a term meta field for the given term ID.
 	 *
-	 * @since 1.33.0
+	 * @since 1.146.0
 	 *
-	 * @param int    $post_id Post ID.
+	 * @param int    $term_id Term ID.
 	 * @param string $key     Metadata key.
 	 * @param mixed  $value   Metadata value. If provided, rows will only be removed that match the value.
 	 * @return bool TRUE on success, otherwise FALSE.
 	 */
-	public function delete( $post_id, $key, $value = '' ) {
-		return delete_post_meta( $post_id, $key, $value );
+	public function delete( $term_id, $key, $value = '' ) {
+		return delete_term_meta( $term_id, $key, $value );
 	}
 }
